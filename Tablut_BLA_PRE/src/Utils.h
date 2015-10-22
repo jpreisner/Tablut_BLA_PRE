450de28c76ff3cbf2fe57984ab7a967ce152056f
gblanc <guillaume.blanchard37@gmail.com>
2015-10-22 18:23:39 +0200

    Utils class

---
+++ Tablut_BLA_PRE/src/Utils.h
@@ -0,0 +1,22 @@
+/*
+ * Utils.h
+ *
+ *  Created on: 22 oct. 2015
+ *      Author: Guillaume
+ */
+
+#ifndef SRC_UTILS_H_
+#define SRC_UTILS_H_
+
+#include <stddef.h>
+
+class Utils {
+public:
+
+	static int getRandomValue(int min, int max){
+		srand(time(NULL));
+		return rand()%(max-min + 1) + min;
+	}
+};
+
+#endif /* SRC_UTILS_H_ */
